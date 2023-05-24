7e2985eee704c205ef7a9b8a90c4fe1b45caca00
Maxime Moreira <maxime.moreira@gmail.com>
2023-05-24 15:03:09 +0200

    with 2 news features

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,7 +10,7 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to restaurant_path(@restaurant), notice: "comment was successfully created."
     else
-      render :new, status: :unprocessable_entity
+      render 'restaurants/show', status: :unprocessable_entity
     end
   end
 
